9023b841f57ce02c45de96a6457c791f8c1718d0
dereck05 <dereckescalante@hotmail.com>
2019-11-16 14:46:02 -0600

    estructura

--- Semantical.cpp
+++ Semantical.cpp
@@ -11,32 +11,25 @@
 #include "Nodo.h"
 using  namespace  std;
 
-vector<VarObject> arbol;
-
-
-void generarArbol(){
-    arbol.push_back(new VarObject("Program","NA","NA","NA"));
-    arbol.push_back(new VarObject("Decls","NA","NA","NA"));
-    arbol.push_back(new VarObject("Decl","NA","NA","NA"));
-    arbol.push_back(new VarObject("ClassDecl","NA","NA","NA"));
-    arbol.push_back(new VarObject("VariableDecl","NA","NA","NA"));
-    arbol.push_back(new VarObject("Variable","int","x","NA"));
-    arbol.push_back(new VarObject("Fields","NA","NA","NA"));
-    arbol.push_back(new VarObject("FunctionDecl","NA","NA","NA"));
-    arbol.push_back(new VarObject("CommaVarList","NA","NA","NA"));
-    arbol.push_back(new VarObject("Variable","int","y","NA"));
-    arbol.push_back(new VarObject("StmtBlock","NA","NA","NA"));
-    arbol.push_back(new VarObject("VariableDecl","NA","NA","NA"));
-    arbol.push_back(new VarObject("Variable","int","var","NA"));
-    arbol.push_back(new VarObject("Expresion","NA","NA","NA"));
-    arbol.push_back(new VarObject("LValue","NA","x","NA"));
-    arbol.push_back(new VarObject("Expresion","NA","NA","NA"));
-    arbol.push_back(new VarObject("INT","NA","NA","2"));
-
-}
 
 
 
-int  main(int  num_args , char** args) {
-    generarArbol();
+void analizadorSemantico(Nodo* arbol){
+  if(arbol == NULL) {
+     return;
+  }
+  switch(arbol->nombre.c_str()){
+    case "WhileStmt":
+      //analizarWhile()
+      break;
+    case "ForStmt":
+      //analizarFor();
+      break;
+
+    default:
+      break;
+  }
+  analizadorSemantico(arbol->first);
+  analizadorSemantico(arbol->second);
+  analizadorSemantico(arbol->third);
 }
